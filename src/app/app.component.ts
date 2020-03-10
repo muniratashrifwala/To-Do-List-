032bbd1a4cc43eb17f9c77271605ecfd4849bc01
muniratashrifwala <munira.i.tashrifwala@gmail.com>
2020-03-10 18:10:33 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'note';
+  todos = [
+    {
+      label: 'Bring Milk',
+      done: false
+    },
+    {
+      label: 'Pay Mobile Bills',
+      done: true
+    },
+    {
+      label: 'Clean House',
+      done: false
+    },
+    {
+      label: "Buy Books",
+      done: true
+    }
+  ];
+
+  addTodo(newTodoLabel) {
+    var newTodo = {
+      label: newTodoLabel,
+      done: false
+    };
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo) {
+    this.todos = this.todos.filter(t => t.label !== todo.label);
+  }
+
+
+
 }
